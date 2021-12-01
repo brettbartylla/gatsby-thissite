483e679bf6cdecb3bd84b934ae2ceaba18d782ad
brett <brett.bartylla@outlook.com>
2021-12-01 17:23:44 -0600

    chore(code): deleted unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,17 +30,3 @@ const IndexPage = ({data}) => (
 )
 
 export default IndexPage
-
-
-//image queries
-// export const query = graphql`
-//   query {
-//     cardGatsbyImage: file(relativePath: {eq: "cards.png"}) {
-//       childImageSharp {
-//         fluid(maxWidth: 2000) {
-//         ...GatsbyImageSharpFluid
-//         }
-//       }
-//     }
-//   },
-// `
