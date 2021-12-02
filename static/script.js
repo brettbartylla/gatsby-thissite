0f19ec6cb703ca2ab9eea71651d950e0b14e0844
brett <brett.bartylla@outlook.com>
2021-12-01 19:36:42 -0600

    feat(audio): randomized playback speed

--- static/script.js
+++ static/script.js
@@ -2,6 +2,25 @@
 var audio = document.getElementById("player");
 audio.play();
 
+//detune after a while
+function randRange(data) {
+  var newTime = data[Math.floor(data.length * Math.random())];
+  return newTime;
+}
+
+function toggleSomething() {
+  var timeArray = new Array(200, 3000, 2000, 2500, 4000, 3000, 5000, 6000);
+  // creates random playback speeds
+  audio.playbackRate = Math.floor(Math.random() * (9 + 9)) / 10;
+  console.log(timer);
+  console.log(audio.playbackRate);
+  clearInterval(timer);
+  timer = setInterval(toggleSomething, randRange(timeArray));
+}
+
+var timer = setInterval(toggleSomething, 35000);
+// 40000 = Initial timer when the page is first loaded
+
 function colorChange() { 
   var i = 0;
   function change() {
@@ -9,6 +28,8 @@ function colorChange() {
     var color = ["red", "blue", "black", "green"];
     document.body.style.backgroundColor = color[i];
     i = (i + 1) % color.length;
+
+
   }
   setInterval(change, 480); //synced to bpm
 }
